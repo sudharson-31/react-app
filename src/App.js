b9f91e981186749a6ed93660e9ef63f858c8c7ae
sudharson-31 <sudharsansri105@outlook.com>
2022-04-20 19:55:56 +0530

    App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Images from './Images';
+function Card(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="main-card">
+          <Profile />
+          <Contact />
+          <Content />
+          <Footer />
+      </div>
+  )
+}
+function Profile(){
+  return (
+      <div className="profile">
+      <div className="profile-dp">
+          <img src= {Images.dp} alt="Profile DP"></img>
+      </div>
+      <div className="profile-details">
+          <h2 id="name">Laura Smith</h2>
+          <span id="role">Frontend Developer</span>
+          <span id="website">laurasmith.website</span>
+      </div>
+      </div>
+
+  )
+}
+function Contact(){
+  return(
+      <div className="contact">
+              <button id="email"><img src={Images.email} alt="email"></img>Email</button>
+              <button id="linkedin"><img src = {Images.linkedin} alt="linkedin"></img>LinkedIn</button>
+      </div>
+  )
+}
+function Content(){
+  return(
+      <div className="content">
+          <h3>About</h3>
+          <content>
+          I am a frontend developer with a particular interest in making things simple and automating daily tasks. 
+          I try to keep up with security and best practices, and am always looking for new things to learn.
+          </content>
+          <h3>Interests</h3>
+          <content>
+          Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. 
+          Pop culture ninja. Coffee fanatic.
+          </content>
+      </div>
+  )
 }
 
-export default App;
+function Footer(){
+  return(
+      <div className="footer">
+          <img src={Images.twitter} alt="twitter"></img>
+          <img src= {Images.facebook} alt="facebook"></img>
+          <img src = {Images.instagram} alt="instagram"></img>
+          <img src = {Images.github}  alt="github"></img>
+      </div>
+  )
+}
+
+export default Card;
---
+++ src/Images.js
@@ -0,0 +1,9 @@
+import dp from './Profile_dp.png';
+import email from './email.png';
+import linkedin from './linkedin.png';
+import facebook from './FacebookIcon.png';
+import instagram from './InstagramIcon.png';
+import github from './GitHubIcon.png';
+import twitter from './TwitterIcon.png';
+
+export default {dp,email, linkedin, facebook, instagram, github, twitter};
