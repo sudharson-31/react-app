51f1b5622f3456d3cc42214bf9abbc96cd750ee9
sudharson-31 <sudharsansri105@outlook.com>
2022-04-25 14:14:21 +0530

    images

--- src/Images.js
+++ src/Images.js
@@ -1,9 +1,9 @@
-import dp from './Profile_dp.png';
-import email from './email.png';
-import linkedin from './linkedin.png';
-import facebook from './FacebookIcon.png';
-import instagram from './InstagramIcon.png';
-import github from './GitHubIcon.png';
-import twitter from './TwitterIcon.png';
+import dp from './public/images/Profile_dp.png';
+import email from './public/images/email.png';
+import linkedin from './public/images/linkedin.png';
+import facebook from './public/images/FacebookIcon.png';
+import instagram from './public/images/InstagramIcon.png';
+import github from './public/images/GitHubIcon.png';
+import twitter from './public/images/TwitterIcon.png';
 
 export default {dp,email, linkedin, facebook, instagram, github, twitter};
