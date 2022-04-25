0ae0743762735490494bc824a7040ad1cc8cc1cd
sudharson-31 <sudharsansri105@outlook.com>
2022-04-25 16:27:45 +0530

    Revert "Updates"
    This reverts commit cdee97ad81633f2ee12574c80ce213e20c3c4347.

---
+++ src/Images.js
@@ -0,0 +1,9 @@
+import dp from './public/images/Profile_dp.png';
+import email from './public/images/email.png';
+import linkedin from './public/images/linkedin.png';
+import facebook from './public/images/FacebookIcon.png';
+import instagram from './public/images/InstagramIcon.png';
+import github from './public/images/GitHubIcon.png';
+import twitter from './public/images/TwitterIcon.png';
+
+export default {dp,email, linkedin, facebook, instagram, github, twitter};
